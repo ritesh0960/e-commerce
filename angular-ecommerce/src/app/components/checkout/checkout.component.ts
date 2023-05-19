62a0a0de74af884c09677035fba8591693b2b40a
Ritesh Raushan <riteshraushan0960@gmail.com>
2023-05-19 20:45:36 +0530

    update in year and date checkout

--- angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -88,6 +88,33 @@ export class CheckoutComponent implements OnInit {
         }
       }
 
+      handleMonthsAndYears():void{
+        const creditCardFormGroup=this.checkoutFormGroup.get('creditCard');
+        const currentYear:number=new Date().getFullYear();
+        const selectedYear:number=Number(creditCardFormGroup.value.expiryYear);
+
+        //if the current year equals selected year then start with the current month
+        let startMonth:number;
+
+        if(currentYear==selectedYear){
+          startMonth=new Date().getMonth()+1;
+        }
+        else
+        {
+          startMonth=1;
+        }
+
+
+        this.formService.getCreditCardMonths(startMonth).subscribe(
+          data=>{
+            console.log("retrived month: " + JSON.stringify(data));
+            this.creditCardMonths=data;
+          }
+        )
+
+
+      }
+
       
 
 }
